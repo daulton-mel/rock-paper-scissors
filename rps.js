d2a5233c0299285ae87685400b4545174520d287
Daulton Melendy <159094438+daulton-mel@users.noreply.github.com>
2024-04-02 20:58:17 -0500

    EDIT index to link js file EDIT js to add computer input

---
+++ rps.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+        computerChoice = "rock";
+    } else if(computerChoice <= 0.67) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice())
