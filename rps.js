46627fd6c450426842675b7b1d4cab51ae6d6fac
Daulton Melendy <159094438+daulton-mel@users.noreply.github.com>
2024-04-03 21:48:58 -0500

    Added game functionality, started to work on loop

--- rps.js
+++ rps.js
@@ -10,4 +10,39 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
+        return 'You tie! Try again!';
+    }
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "scissors") {
+            return "You win! Rock beats scissors."
+        } else {
+            return "You lose! Paper beats rock."
+        }
+    }
+    if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper beats rock."
+        } else {
+            return "You lose! Scissors beats paper."
+        }
+    }
+    if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "paper") {
+            return "You win! Scissors beats paper."
+        } else {
+            return "You lose! Rock beats scissors."
+        }
+    }
+}
+
+  const playerSelection = prompt("Please pick: Rock, Paper, or Scissors");
+  const computerSelection = getComputerChoice();
+
+
+function playGame() {
+    for (let i = 0; i < 5; i++ ) {
+        
+    }
+}
