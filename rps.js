886fde2741f37e5d1aafa47ae7171eb7b35d300a
Daulton Melendy <159094438+daulton-mel@users.noreply.github.com>
2024-04-16 20:43:59 -0500

    Added win counter and declaration of the winner

--- rps.js
+++ rps.js
@@ -10,28 +10,37 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === computerSelection) {
         return 'You tie! Try again!';
     }
     if (playerSelection.toLowerCase() === "rock") {
         if (computerSelection === "scissors") {
+            playerScore ++
             return "You win! Rock beats scissors."
         } else {
+            computerScore ++
             return "You lose! Paper beats rock."
         }
     }
     if (playerSelection.toLowerCase() === "paper") {
         if (computerSelection === "rock") {
+            playerScore ++
             return "You win! Paper beats rock."
         } else {
+            computerScore ++
             return "You lose! Scissors beats paper."
         }
     }
     if (playerSelection.toLowerCase() === "scissors") {
         if (computerSelection === "paper") {
+            playerScore ++
             return "You win! Scissors beats paper."
         } else {
+            computerScore ++
             return "You lose! Rock beats scissors."
         }
     }
@@ -42,6 +51,11 @@ function playGame() {
         const playerSelection = prompt("Please pick: Rock, Paper, or Scissors");
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection))
+    }
+    if (playerScore > computerScore) {
+        return console.log('You Won!')
+    } else {
+        return console.log('The Computer Wins!')
     }
 }
 
